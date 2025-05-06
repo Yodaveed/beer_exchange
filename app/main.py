c6aed9b8721c0a96a9557d0aded31dfc30f8fa85
David Gregory <yodaveed@gmail.com>
2025-05-06 13:25:12 -0400

    Reorganize project structure

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from app.routers import drinks
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI is alive"}
-
-app.include_router(drinks.router)
-
+    return {"message": "FastAPI is working"}
