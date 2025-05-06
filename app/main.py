c997a1e549e399a02d5665748110d93e4d4fa6a1
David Gregory <yodaveed@gmail.com>
2025-05-06 13:08:25 -0400

    Fix module import and add init files

--- app/main.py
+++ app/main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 from app.routers import drinks
 
 app = FastAPI()
-app.include_router(drinks.router)
+
 @app.get("/")
 def read_root():
     return {"message": "FastAPI is alive"}
+
+app.include_router(drinks.router)
+
