15dba9fc4fef03b322584f2a23f3449e08b9c0d3
David Gregory <yodaveed@gmail.com>
2025-05-06 17:26:38 -0400

    Add beer dashboard

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
-
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.routers import drinks
 
 app = FastAPI(title="The Beer Exchange")
 
 app.include_router(drinks.router)
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 @app.get("/")
 def root():
     return {"message": "FastAPI is working!"}
+
+@app.get("/dashboard")
+def get_dashboard():
+    return FileResponse("app/static/dashboard.html")
